95dd74bf3df6dfe95511c14cfb93660208ce8521
Rishabh Khurana <rishabhk994@gmail.com>
2024-03-11 00:12:07 +1100

    shifting keymaps

--- init.lua
+++ init.lua
@@ -375,7 +375,12 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
+      -- For shifting lines
+      vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+      vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+      vim.keymap.set('n', 'U', '<C-R>')
+      vim.keymap.set('n', 'B', '<C-u>')
+      vim.keymap.set('n', 'F', '<C-d>')
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to telescope to change theme, layout, etc.
