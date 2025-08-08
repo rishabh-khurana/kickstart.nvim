aefe4cd4239873ff07bafe6ba1d666ef7d8a8fdd
Rishabh Khurana <rishabhk994@gmail.com>
2025-08-09 01:10:41 +1000

    Update init.lua
    added avante nvim

--- init.lua
+++ init.lua
@@ -206,7 +206,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
 })
 
@@ -731,7 +731,7 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
       })
-      -- require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
         ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
@@ -967,6 +967,102 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
+  -- {
+  --   "yetone/avante.nvim",
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   -- ⚠️ must add this setting! ! !
+  --   build = 'make BUILD_FROM_SOURCE=true',
+  --   event = "VeryLazy",
+  --   version = false, -- Never set this value to "*"! Never!
+  --   ---@module 'avante'
+  --   ---@type avante.Config
+  --   opts = {
+  --     provider = 'atlopenai', -- Changed to use gemini as default
+  --     providers = {
+  --       -- Google Gemini provider
+  --       -- Keep your existing atlassian openAI provider as well
+  --       atlopenai = {
+  --         __inherited_from = 'openai',
+  --         endpoint = 'https://ai-gateway.sgw.prod.atl-paas.net/v1/openai/v1/chat/completions',
+  --         api_key_name = '',
+  --         model = 'gpt-4o-2024-11-20',
+
+  --         parse_curl_args = function(opts, code_opts)
+  --           local openai = require 'avante.providers.openai'
+
+  --           -- local gemini = require 'avante.providers.gemini'
+  --           local handle = io.popen 'atlas slauth token --env=prod --aud=ai-gateway'
+  --           local output = handle:read '*a'
+  --           local token = output:gsub('[\n\r]', ' ')
+  --           handle:close()
+
+  --           local headers = {
+  --             ['Content-Type'] = 'application/json',
+  --             ['Authorization'] = 'SLAUTH ' ..token,
+  --             ['X-Atlassian-UseCaseId'] = 'avante-nvim',
+  --             ['X-Atlassian-CloudId'] = 'a436116f-02ce-4520-8fbb-7301462a1674',
+  --           }
+            
+  --           local tools = nil
+  --           tools = {}
+  --           for _, tool in ipairs(code_opts.tools) do
+  --             table.insert(tools, openai:transform_tool(tool))
+  --           end
+
+  --           return {
+  --             url = 'https://ai-gateway.sgw.prod.atl-paas.net/v1/openai/v1/chat/completions',
+  --             headers = headers,
+  --             body = vim.tbl_deep_extend('force', {
+  --               model = 'gpt-4o-2024-11-20',
+  --               messages = openai:parse_messages(code_opts),
+  --               stream = true,
+  --               tools = tools,
+  --             }, {}),
+  --           }
+  --         end,
+  --       },
+  --     },
+  --   },
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "echasnovski/mini.pick", -- for file_selector provider mini.pick
+  --     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+  --     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+  --     "ibhagwan/fzf-lua", -- for file_selector provider fzf
+  --     "stevearc/dressing.nvim", -- for input provider dressing
+  --     "folke/snacks.nvim", -- for input provider snacks
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     "zbirenbaum/copilot.lua", -- for providers='copilot'
+  --     {
+  --       -- support for image pasting
+  --       "HakonHarnes/img-clip.nvim",
+  --       event = "VeryLazy",
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       'MeanderingProgrammer/render-markdown.nvim',
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- },
+    
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
